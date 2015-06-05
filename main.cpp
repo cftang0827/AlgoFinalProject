4335a1e1a1ac2642b0273b5b61c7ff624220a281
cftang0827 <cftang0827@gmail.com>
2015-06-06 01:46:51 +0800

    temp version in 2015/6/6

--- main.cpp
+++ main.cpp
@@ -33,9 +33,9 @@ bool myfunction4(Edge* a,Edge* b);
 DataList list;
 string alpha, beta, omega;
 int Alpha, Beta, Omega;
-vector<Graph*> graphArray;
-vector<Node*> nodeArray;
-vector<Edge*> edgeArray;
+vector<Graph*> graphVector;
+vector<Node*> nodeVector;
+vector<Edge*> edgeVector;
 
 
 
@@ -43,16 +43,19 @@ int main()
 {
 	getData();
 	sortDataList(list);
-//	for(int i = 0;i<list.size();i++)
-//	{
-//		for(int j=0;j<list[i].size();j++)
-//		{
-//			cout<<setw(5)<<list[i][j]<<" ";
-//		}
-//		cout<<endl;
-//	}
+
+    cout<<"show Node: =========="<<endl;
+    for(int i = 0;i<list.size();i++)
+    {
+        for(int j=0;j<list[i].size();j++)
+        {
+            cout<<setw(5)<<list[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
 	Graph a22("ss") ;
+    cout<<"show Parameter: =========="<<endl;
 	cout<<"alpha: "<<alpha<<endl;
 	cout<<"beta: "<<beta<<endl;
 	cout<<"omega: "<<omega<<endl; 
@@ -68,48 +71,75 @@ int main()
 
     makeEdge();
     makeGraph();
-//	cout<<"node array size: "<<nodeArray.size()<<endl;
-//	cout<<"graph array size: "<<graphArray.size()<<endl;
+//	cout<<"node array size: "<<nodeVector.size()<<endl;
+//	cout<<"graph array size: "<<graphVector.size()<<endl;
 
-//    cout<<"show edge: "<<endl;
-//    sort(edgeArray.begin(),edgeArray.end(),myfunction4);
-//    for(int i=0;i<edgeArray.size();i++)
-//    {
-//        cout<<edgeArray[i]->node[0]->id<<", "<<edgeArray[i]->node[1]->id<<endl;
-//    }
+    cout<<"show Edge: =========="<<endl;
+    sort(edgeVector.begin(),edgeVector.end(),myfunction4);
+    for(int i=0;i<edgeVector.size();i++)
+    {
+        cout<<edgeVector[i]->node[0]->id<<", "<<edgeVector[i]->node[1]->id<<endl;
+    }
 
 //    cout<<"----------------------------------"<<endl;
-//    cout<<"graph size: "<<graphArray.size()<<endl;
-//    for(int i = 0; i< graphArray.size();i++)
+//    cout<<"graph size: "<<graphVector.size()<<endl;
+//    for(int i = 0; i< graphVector.size();i++)
 //    {
-//        for(int j = 0; j<graphArray[i]->nodes.size();j++)
+//        for(int j = 0; j<graphVector[i]->nodes.size();j++)
 //        {
-//            cout<<graphArray[i]->nodes[j]->id<<" ";
+//            cout<<graphVector[i]->nodes[j]->id<<" ";
 //        }
 //        cout<<endl;
 //    }
 
-
-    for(int i = 0; i< graphArray.size(); i++)
+    cout<<"Show Part: =========="<<endl;
+    for(int i = 0; i< graphVector.size(); i++)
     {
-        for(int j = 0; j<graphArray[i]->nodes.size(); j++)
+        for(int j = 0; j<graphVector[i]->nodes.size(); j++)
         {
-//            cout<<graphArray[i]->nodes[j]->left<<",";
-//            cout<<graphArray[i]->nodes[j]->down<<",";
-//            cout<<graphArray[i]->nodes[j]->right<<",";
-//            cout<<graphArray[i]->nodes[j]->top<<endl;
-//            cout<<graphArray[i]->nodes[j]->id<<endl;
-            int ttt = graphArray[i]->nodes[j]->id-1;
-            cout<<nodeArray[ttt]->left<<",";
-            cout<<nodeArray[ttt]->down<<",";
-            cout<<nodeArray[ttt]->right<<",";
-            cout<<nodeArray[ttt]->top<<endl;
+//            cout<<graphVector[i]->nodes[j]->left<<",";
+//            cout<<graphVector[i]->nodes[j]->down<<",";
+//            cout<<graphVector[i]->nodes[j]->right<<",";
+//            cout<<graphVector[i]->nodes[j]->top<<endl;
+//            cout<<graphVector[i]->nodes[j]->id<<endl;
+            int ttt = graphVector[i]->nodes[j]->id-1;
+            graphVector[i]->nodes[j]->left = nodeVector[ttt]->left;
+            graphVector[i]->nodes[j]->right = nodeVector[ttt]->right;
+            graphVector[i]->nodes[j]->top = nodeVector[ttt]->top;
+            graphVector[i]->nodes[j]->down = nodeVector[ttt]->down;
+
+            cout<<nodeVector[ttt]->left<<",";
+            cout<<nodeVector[ttt]->down<<",";
+            cout<<nodeVector[ttt]->right<<",";
+            cout<<nodeVector[ttt]->top<<endl;
+
+//            cout<<"===================="<<endl;
+//                        cout<<graphVector[i]->nodes[j]->left<<",";
+//                        cout<<graphVector[i]->nodes[j]->down<<",";
+//                        cout<<graphVector[i]->nodes[j]->right<<",";
+//                        cout<<graphVector[i]->nodes[j]->top<<endl;
+//                        cout<<graphVector[i]->nodes[j]->id<<endl;
 
         }
         cout<<endl;
 //        cout<<endl;
     }
-	
+
+
+    cout<<"================"<<endl;
+    for(int i = 0; i< graphVector.size(); i++)
+    {
+        for(int j = 0; j<graphVector[i]->nodes.size(); j++)
+        {
+            //            cout<<"===================="<<endl;
+              cout<<graphVector[i]->nodes[j]->left<<",";
+              cout<<graphVector[i]->nodes[j]->down<<",";
+              cout<<graphVector[i]->nodes[j]->right<<",";
+              cout<<graphVector[i]->nodes[j]->top<<endl;
+
+        }
+        cout<<endl;
+    }
 }
 
 
@@ -117,7 +147,7 @@ int main()
 //get data from test.txt to DataList datastructure
 void getData()
 {
-    fstream fs ("..//test.txt" ,fstream::in);
+    fstream fs ("..//iccad2015_input.case3" ,fstream::in);
 	string line;
 	
 	while(getline(fs,line))
@@ -127,11 +157,11 @@ void getData()
 		num.clear();
 		Data d;
 		d.clear();
-		if(a>=48 && a<=57)
+        if((a>=48 && a<=57) || a == 45)
 		{
 			for(int i=0; i< line.size(); i++)
 			{
-				if((int)line[i]>=48 && (int)line[i]<=57 && i < line.size()-1)
+                if(((int)line[i]>=48 && (int)line[i]<=57 && i < line.size()-1) || (int)line[i]==45  )
 				{
 					num.push_back(line[i]);
 				}else if((int)line[i] == 44 )
@@ -145,7 +175,7 @@ void getData()
 					num.clear();
 					list.push_back(d);
 					d.clear();
-				}
+                }
 			}
 		}
 		else if(a==65)
@@ -208,14 +238,138 @@ void makeNode(DataList& in)
 		Node* temp = new Node(i+1);
 		stringstream left1,down1,top1,right1;
 		int left2,down2,top2,right2;
-		left1<<in[i][0];
-		down1<<in[i][1];
-		top1<<in[i][3];
-		right1<<in[i][2];
-		left1>>left2;
-		down1>>down2;
-		right1>>right2;
-		top1>>top2;
+
+        if(in[i][0][0] == '-')
+        {
+            string temp0 = in[i][0];
+            temp0.erase(temp0.begin());
+            left1<<temp0;
+            left1>>left2;
+            left2 = (-1)*left2;
+        }
+        else
+        {
+            left1<<in[i][0];
+            left1>>left2;
+        }
+
+        if(in[i][1][0] == '-')
+        {
+            string temp1 = in[i][1];
+            temp1.erase(temp1.begin());
+            down1<<temp1;
+            down1>>down2;
+            down2 = (-1)*down2;
+        }else
+        {
+            down1<<in[i][1];
+            down1>>down2;
+        }
+
+        if(in[i][3][0] == '-')
+        {
+            string temp3 = in[i][3];
+            temp3.erase(temp3.begin());
+            top1<<temp3;
+            top1>>top2;
+            top2 = (-1)*top2;
+        }else
+        {
+            top1<<in[i][3];
+            top1>>top2;
+        }
+
+        if(in[i][2][0] == '-')
+        {
+            string temp2 = in[i][2];
+            temp2.erase(temp2.begin());
+            right1<<temp2;
+            right1>>right2;
+            right2 = (-1)*right2;
+        }else
+        {
+            right1<<in[i][2];
+            right1>>right2;
+        }
+
+
+
+
+
+//        if(in[i][2][0] != '-' || in[i][0][0] != '-' || in[i][3][0] != '-' || in[i][1][0] != '-')
+//        {
+//            left1<<in[i][0];
+//            down1<<in[i][1];
+//            top1<<in[i][3];
+//            right1<<in[i][2];
+
+
+//            left1>>left2;
+//            down1>>down2;
+//            right1>>right2;
+//            top1>>top2;
+//        }
+
+
+
+
+
+//        left1>>left2;
+//		down1>>down2;
+//		right1>>right2;
+//		top1>>top2;
+
+//        cout<<"in[i][0] = "<<in[i][0]<<endl;
+//        cout<<"right2= "<<right2<<endl;
+
+
+
+
+////        cout<<"12341234               "<<in[i][0][0]<<endl;
+//        if(in[i][0][0] == '-')
+//        {
+//            in[i][0].erase(0);
+////            cout<<"fuck"<<endl;
+////            left2 = (-1)*left2;
+//        }else if(in[i][1][0] == '-')
+//        {
+//            in[i][1].erase(0);
+////            cout<<"fuck"<<endl;
+////            down2 = (-1)*down2;
+//        }else if(in[i][3][0] == '-')
+//        {
+//            in[i][3].erase(0);
+////            cout<<"fuck"<<endl;
+////            top2 = (-1)*top2;
+//        }else if(in[i][2][0] == '-')
+//        {
+//            in[i][2].erase(0);
+////            cout<<"fuck"<<endl;
+////            right2 = (-1)*right2;
+//        }else
+//        {
+
+//            left1<<in[i][0];
+//            down1<<in[i][1];
+//            top1<<in[i][3];
+//            right1<<in[i][2];
+//            left1>>left2;
+//            down1>>down2;
+//            right1>>right2;
+//            top1>>top2;
+
+//        }
+
+//        cout<<"left2 = "<<left2<<endl;
+//        cout<<"right2 = "<<right2<<endl;
+//        cout<<"top2 = "<<top2<<endl;
+//        cout<<"down2 = "<<down2<<endl;
+//        cout<<endl;
+        cout<<"rectangle('Position',["<<left2<<" "<<down2;
+        cout<<" "<<abs(right2 - left2)<<" ";
+        cout<<abs(top2-down2)<<"])"<<endl;
+
+
 
 //         cout<<"left2================= "<<left2<<endl;
 //         cout<<"right2================= "<<right2<<endl;
@@ -224,7 +378,7 @@ void makeNode(DataList& in)
 		temp->top = top2;
 		temp->right = right2;
 		
-		nodeArray.push_back(temp);
+        nodeVector.push_back(temp);
 	}
 }
 
@@ -234,37 +388,37 @@ void makeEdge()
 //	Graph* temp = new Graph("1");
 	bool flag;
 	int i;
-	for(i = 0; i < nodeArray.size()-1; i++)
+    for(i = 0; i < nodeVector.size()-1; i++)
     {
-//        cout<<"@@@@@@@@@@@@@@@@@@@@@@@"<<nodeArray[i]->left<<endl;
+//        cout<<"@@@@@@@@@@@@@@@@@@@@@@@"<<nodeVector[i]->left<<endl;
 //        cout<<"iteration "<<i+1<<endl;
 		flag = false;
-		for(int j = i+1; j<nodeArray.size(); j++)
+        for(int j = i+1; j<nodeVector.size(); j++)
 		{	
-			if(overlap(nodeArray[i],nodeArray[j]))
+            if(overlap(nodeVector[i],nodeVector[j]))
 			{
-				if(checkAlpha(nodeArray[i], nodeArray[j])  )
+                if(checkAlpha(nodeVector[i], nodeVector[j]) )
 				{
 //                    cout<<"checkalpha OK"<<"i="<<i+1<<"j="<<j+1<<endl;
-					if(!x_overlap(nodeArray[i], nodeArray[j]))
+                    if(!x_overlap(nodeVector[i], nodeVector[j]))
 					{
-                        Edge* t = new Edge(nodeArray[i],nodeArray[j]);
-//						temp->addEdge(nodeArray[i]->id,nodeArray[j]->id);
-                        edgeArray.push_back(t);
+                        Edge* t = new Edge(nodeVector[i],nodeVector[j]);
+//						temp->addEdge(nodeVector[i]->id,nodeVector[j]->id);
+                        edgeVector.push_back(t);
 //                        cout<<"x----"<<endl;
 //                        cout<<"i= "<<i+1<<endl;
 //                        cout<<"j= "<<j+1<<endl;
 						flag = true;
 					}
 				}
-				if(checkBeta(nodeArray[i], nodeArray[j]))
+                if(checkBeta(nodeVector[i], nodeVector[j]))
 				{
 //                    cout<<"checkbeta OK"<<"i="<<i+1<<"j="<<j+1<<endl;
-					if(!y_overlap(nodeArray[i], nodeArray[j]))
+                    if(!y_overlap(nodeVector[i], nodeVector[j]))
 					{
-                         Edge* t = new Edge(nodeArray[i],nodeArray[j]);
-//                        temp->addEdge(nodeArray[i]->id,nodeArray[j]->id);
-                         edgeArray.push_back(t);
+                         Edge* t = new Edge(nodeVector[i],nodeVector[j]);
+//                        temp->addEdge(nodeVector[i]->id,nodeVector[j]->id);
+                         edgeVector.push_back(t);
 //                        cout<<"y----"<<endl;
 //                        cout<<"i= "<<i+1<<endl;
 //                        cout<<"j= "<<j+1<<endl;
@@ -282,12 +436,12 @@ void makeEdge()
 //		if(!flag)
 //		{
 //			cout<<"lalalalalalalallalalalalalala"<<endl;
-//			graphArray.push_back(temp);
+//			graphVector.push_back(temp);
 //			temp = new Graph("2");
 //		}
 	}
 //	cout<<"size "<<temp->edges.size()<<endl;
-//    cout<<"edgeSize: "<<edgeArray.size()<<endl;
+//    cout<<"edgeSize: "<<edgeVector.size()<<endl;
 }
 
 
@@ -409,22 +563,22 @@ bool checkEnd(Node* a)
     bool flag1 = true;
     for(int i = 0; i< a->id-1; i++)
     {
-        for(int j = a->id+1; j< nodeArray.size(); j++)
+        for(int j = a->id+1; j< nodeVector.size(); j++)
         {
-            if(overlap(nodeArray[i],nodeArray[j]))
+            if(overlap(nodeVector[i],nodeVector[j]))
             {
-                if(checkBeta(nodeArray[i],nodeArray[j]))
+                if(checkBeta(nodeVector[i],nodeVector[j]))
                 {
-                    if(!x_overlap(nodeArray[i], nodeArray[j]))
+                    if(!x_overlap(nodeVector[i], nodeVector[j]))
                     {
                         flag1 = false;
 //                        cout<<"checkEnd at(beta) "<<i+1<<","<<j+1<<endl;
 
                     }
                 }
-                if(checkAlpha(nodeArray[i],nodeArray[j]))
+                if(checkAlpha(nodeVector[i],nodeVector[j]))
                 {
-                    if(!y_overlap(nodeArray[i], nodeArray[j]))
+                    if(!y_overlap(nodeVector[i], nodeVector[j]))
                     {
                         flag1 = false;
 //                        cout<<"checkEnd at(alpha) "<<i+1<<","<<j+1<<endl;
@@ -441,53 +595,76 @@ bool checkEnd(Node* a)
 
 void makeGraph()
 {
-    sort(edgeArray.begin(),edgeArray.end(),myfunction4);
+    bool *checkIfInGraph = new bool[nodeVector.size()];
+    for(int i = 0; i<nodeVector.size();i++)
+    {
+        checkIfInGraph[i] = false;
+        cout<<"YOYOYOYOYOYOYOYOYO"<<endl;
+    }
+    sort(edgeVector.begin(),edgeVector.end(),myfunction4);
 //    Graph* temp = new Graph("0");
 //    temp->addEdge(e);
     vector<Node*> checker;
-//    temp->addEdge(edgeArray[0]->node[0]->id,edgeArray[0]->node[1]->id);
-    checker.push_back(edgeArray[0]->node[0]);
-    checker.push_back(edgeArray[0]->node[1]);
+//    temp->addEdge(edgeVector[0]->node[0]->id,edgeVector[0]->node[1]->id);
+    checker.push_back(edgeVector[0]->node[0]);
+    checker.push_back(edgeVector[0]->node[1]);
     Graph* temp = new Graph("0");
-    for(int i = 1; i<edgeArray.size();i++)
+    temp->addEdge(edgeVector[0]->node[0]->id,edgeVector[0]->node[1]->id);
+    checkIfInGraph[edgeVector[0]->node[0]->id] = true;
+    checkIfInGraph[edgeVector[0]->node[1]->id] = true;
+
+
+    for(int i = 1; i<edgeVector.size();i++)
     {
-//        cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~"<<edgeArray[i]->node[0]->left<<endl;
-//        cout<<"----------aaa-------------"<<edgeArray[i]->node[0]->id<<", "<<edgeArray[i]->node[1]->id<<endl;
+//        cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~"<<edgeVector[i]->node[0]->left<<endl;
+//        cout<<"----------aaa-------------"<<edgeVector[i]->node[0]->id<<", "<<edgeVector[i]->node[1]->id<<endl;
         bool flag = false;
         for(int j = 0; j<i ; j++)
         {
-            if(edgeArray[i]->node[0]->id == edgeArray[j]->node[0]->id || edgeArray[i]->node[0]->id == edgeArray[j]->node[1]->id || edgeArray[i]->node[1]->id == edgeArray[j]->node[0]->id || edgeArray[i]->node[1]->id == edgeArray[j]->node[1]->id)
+            if(edgeVector[i]->node[0]->id == edgeVector[j]->node[0]->id || edgeVector[i]->node[0]->id == edgeVector[j]->node[1]->id || edgeVector[i]->node[1]->id == edgeVector[j]->node[0]->id || edgeVector[i]->node[1]->id == edgeVector[j]->node[1]->id)
             {
 
-                temp->addEdge(edgeArray[i]->node[0]->id,edgeArray[i]->node[1]->id);
+                temp->addEdge(edgeVector[i]->node[0]->id,edgeVector[i]->node[1]->id);
 //                cout<<"FUCK OFF!"<<endl;
+                checkIfInGraph[edgeVector[i]->node[0]->id] = true;
+                checkIfInGraph[edgeVector[i]->node[1]->id] = true;
                 flag = true;
                 goto yo;
             }
 
         }
 
-
-
-
-
         yo:
         if(!flag)
         {
 
-            graphArray.push_back(temp);
+            graphVector.push_back(temp);
             temp = new Graph("1");
-            temp->addEdge(edgeArray[i]->node[0]->id,edgeArray[i]->node[1]->id);
+            temp->addEdge(edgeVector[i]->node[0]->id,edgeVector[i]->node[1]->id);
+            checkIfInGraph[edgeVector[i]->node[0]->id] = true;
+            checkIfInGraph[edgeVector[i]->node[1]->id] = true;
 //            cout<<"FUCK OFF!"<<endl;
 
         }
 
     }
-    graphArray.push_back(temp);
+    graphVector.push_back(temp);
+
+    for(int i = 0; i<nodeVector.size(); i++)
+    {
+        if(!checkIfInGraph[i])
+        {
+            temp = new Graph("5");
+            temp->nodes.push_back(nodeVector[i]);
+            graphVector.push_back(temp);
+        }
+    }
+
+
 
 }
 
 bool myfunction4(Edge* a,Edge* b)
 {
-    return (a->node[0]<b->node[0]);
+    return (a->node[0]->id<b->node[0]->id);
 }
